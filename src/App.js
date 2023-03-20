6dd527ea8a805b726df73510f1650624af755004
Yukio <DivinusMessor@users.noreply.github.com>
2023-03-20 16:55:28 -0700

    Assignment 3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Gradebook from './components/Gradebook';
 import Assignment from './components/Assignment';
+import createAssignment from './components/createAssignment';
 import {BrowserRouter, Switch,  Route} from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -20,8 +23,10 @@ function App() {
        <Switch>
         <Route exact path='/' component={Assignment} />
         <Route path='/gradebook' component={Gradebook} />
+        <Route path='/createAssignment' component={createAssignment} />
        </Switch>
       </BrowserRouter>
+      <ToastContainer autoClose={1500}/>
     </div>
   );
 }
---
+++ src/components/createAssignment.js
@@ -0,0 +1,120 @@
+// import React from 'react';
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom'
+import Cookies from 'js-cookie';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import {DataGrid} from '@mui/x-data-grid';
+import {SERVER_URL} from '../constants.js'
+import { TextField } from '@mui/material';
+
+
+import { withRouter } from 'react-router-dom';
+
+class createAssignment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      dueDate: '',
+      courseId: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name, dueDate, courseId } = this.state;
+    fetch(`${SERVER_URL}/assignment/add/${name}/${dueDate}/${courseId}`, {
+        method: 'POST',
+      })
+      .then(res => {
+        if (res.ok) {
+        toast.success("Assignment Successfully Added!", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        this.props.history.push('/');
+      } else {
+        toast.error("Assignment NOT Added", {
+          position: toast.POSITION.BOTTOM_LEFT
+      });
+    }})
+  }
+
+  render() {
+    const { name, dueDate, courseId } = this.state;
+
+const formStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '10px',
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+};
+
+const buttonStyle = {
+  backgroundColor: '#4CAF50',
+  color: 'white',
+  border: 'none',
+  padding: '10px 20px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  margin: '4px 2px',
+  cursor: 'pointer',
+  borderRadius: '4px',
+};
+
+return (
+  <div style={containerStyle}>
+    <h2>Create New Assignment</h2>
+    <form onSubmit={this.handleSubmit} style={formStyle}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={this.handleChange}
+        required
+      />
+      <label htmlFor="dueDate">Due Date:</label>
+      <input
+        type="date"
+        id="dueDate"
+        name="dueDate"
+        value={dueDate}
+        onChange={this.handleChange}
+        required
+      />
+      <label htmlFor="courseId">Course ID:</label>
+      <input
+        type="text"
+        id="courseId"
+        name="courseId"
+        value={courseId}
+        onChange={this.handleChange}
+        required
+      />
+      <button type="submit" style={buttonStyle}>Create Assignment</button>
+    </form>
+  </div>
+);
+
+  }
+}
+
+export default withRouter(createAssignment);
